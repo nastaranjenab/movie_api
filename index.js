53f322b07f6a76ee3563b04f0aef3f163d6b52f4
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-05-02 19:47:56 +0200

    corrected Authorization

--- index.js
+++ index.js
@@ -143,33 +143,31 @@ app.get('/movies/directors/:Name', passport.authenticate('jwt', { session: false
 
 
 
-
 //Add a user and register
-app.post('/users', passport.authenticate('jwt', { session: false }),
-(req, res) => {
+app.post('/users', (req, res) => {
   Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-      } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
+      .then((user) => {
+          if(user) {
+              return res.status(400).send(req.body.Username + ' already exists!');
+          }
+          else {
+              Users.create({
+                  Username: req.body.Username,
+                  Password: req.body.Password,
+                  Email: req.body.Email,
+                  Birthday: req.body.Birthday
+              })
+                  .then((user) => {res.status(201).json(user)})
+                  .catch((err) => {
+                      console.error(err);
+                      res.status(500).send('Error: ' + err);
+                  });
+          }
+      })
+      .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+      });
 });
 
 //update

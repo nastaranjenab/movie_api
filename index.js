3f44144d70eb647065b0219e3d303ab211d322b7
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-06-01 20:42:53 +0200

    updated herokuname

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017/movieapp', {
  });*/
 // To allow certain origins to be given access to make requests
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movieworldnast.herokuapp.com/'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movieflexworld.herokuapp.com/'];
 
 app.use(cors({
   origin: (origin, callback) => {

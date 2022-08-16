6784d960cc0d5295c5a00fb5613b395e9bcee146
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-08-16 21:38:27 +0200

    added endpoint Login

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://localhost:27017/movieapp', {
  });*/
 // To allow certain origins to be given access to make requests
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movieflexworld.herokuapp.com/','https://flourishing-horse-be1ece.netlify.app/'
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movieflexworld.herokuapp.com/','https://flourishing-horse-be1ece.netlify.app/login'
 ,'https://movieflexworld.herokuapp.com/login'];
 
 app.use(cors({
